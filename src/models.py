306bcfd87fab25945da2d42e839682237d104f34
Carlos Daniel Ponce <135666468+carlosp-11@users.noreply.github.com>
2023-10-07 12:44:33 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,66 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Users(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    full_name = Column(String(250))
+    email = Column(String(250))  # ¿existe un tipo 'email' que contenga @ y verifique si el dato es un correo?
+    password = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    population = Column(Integer)
+    climate = Column(String(250))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    birth_date = Column(Integer)
+    gender = Column(String(20))
+    skin_color = Column(String(20))
+    eyes_color = Column(String(20))
+    hair_color = Column(String(20))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    vehicle_class = Column(String(50))
+    model = Column(String(50))
+    max_atmosferical_speed= Column(Integer)
+    crew = Column(Integer)
+    length = Column(Integer)
+
+class FavoritePlanets(Base):
+    __tablename__ = 'favorite_planets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    user = relationship(Users)
+    planet = relationship(Planets)
+
+class FavoriteCharacters(Base):
+    __tablename__ = 'favorite_characters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    user = relationship(Users)
+    character = relationship(Characters)
+
+class FavoriteVehicles(Base):
+    __tablename__ = 'favorite_vehicles'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    user = relationship(Users)
+    vehicle = relationship(Vehicles)
 
     def to_dict(self):
         return {}
